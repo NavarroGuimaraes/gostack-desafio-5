06d5e39ffe72b5b2aeab5f28070a447eab39c4cb
Navarro Silva <navasxll@hotmail.com>
2020-05-01 23:13:41 -0300

    adding todo features

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,55 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+
+  private INCOME_TYPE = 'income';
+
+  private OUTCOME_TYPE = 'outcome';
 
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+
+    this.transactions.forEach(transaction => {
+      if (transaction.type === this.INCOME_TYPE) {
+        income += transaction.value;
+      } else if (transaction.type === this.OUTCOME_TYPE) {
+        outcome += transaction.value;
+      }
+    });
+
+    const total = income - outcome;
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,15 +1,44 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
+
+  private OUTCOME_TYPE = 'outcome';
+
+  private INCOME_TYPE = 'income';
 
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (type === this.OUTCOME_TYPE) {
+      const actualBalance = this.transactionsRepository.getBalance();
+      if (actualBalance.total - value < 0) {
+        throw Error(
+          'The value is below zero, therefore your outcome cannot be executed',
+        );
+      }
+    } else if (!(type === this.INCOME_TYPE)) {
+      // if it's here, then it's not outcome
+      // so it must be income, therefore if it's not income, its wrong!
+      throw Error('The type must be only outcome or income');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
